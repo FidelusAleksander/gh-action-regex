d60001252c719e8815335d6662339cde66a26d8e
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-04-09 16:01:14 +0200

    v0.2.0 updates

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ class Action:
     SUPPORTED_REGEX_MATCH_TYPES = ["search", "fullmatch", "match"]
 
     def __init__(self, regex_pattern: str, text: str, regex_match_type: str):
-        self.REGEX_MATCH_TYPE = regex_match_type
-        self.REGEX_PATTERN = regex_pattern
-        self.TEXT = text
+        self.regex_match_type = regex_match_type
+        self.regex_pattern = regex_pattern
+        self.text = text
 
         self._validate_input()
 
@@ -23,21 +23,21 @@ class Action:
         )
 
     def _validate_input(self):
-        if self.REGEX_MATCH_TYPE not in self.SUPPORTED_REGEX_MATCH_TYPES:
+        if self.regex_match_type not in self.SUPPORTED_REGEX_MATCH_TYPES:
             print(
-                f"Regex match type {self.REGEX_MATCH_TYPE} has to be one of {self.SUPPORTED_REGEX_MATCH_TYPES}"
+                f"Regex match type {self.regex_match_type} has to be one of {self.SUPPORTED_REGEX_MATCH_TYPES}"
             )
             sys.exit(1)
 
     def _print_result(self, successful: bool):
         body = vars(self)
-        body["MATCH"] = successful
+        body["match"] = successful
         print(json.dumps(body))
-        print(f"::set-output name=MATCH::{json.dumps(successful)}")
+        print(f"::set-output name=match::{json.dumps(successful)}")
 
     def run(self):
-        result = getattr(re, self.REGEX_MATCH_TYPE)(
-            pattern=self.REGEX_PATTERN, string=self.TEXT
+        result = getattr(re, self.regex_match_type)(
+            pattern=self.regex_pattern, string=self.text
         )
         self._print_result(bool(result))
         sys.exit(0 if result else 1)
