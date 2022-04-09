237b51a729e070ad1a3ad460f5dd582708b09348
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2022-04-09 11:13:09 +0200

    Test.py added
    Pre-commit lint run

--- main.py
+++ main.py
@@ -16,22 +16,29 @@ class Action:
 
     @classmethod
     def from_environ(cls):
-        return cls(regex_pattern=os.environ["INPUT_REGEX_PATTERN"], text=os.environ["INPUT_TEXT"],
-                   regex_match_type=os.environ["INPUT_REGEX_MATCH_TYPE"])
+        return cls(
+            regex_pattern=os.environ["INPUT_REGEX_PATTERN"],
+            text=os.environ["INPUT_TEXT"],
+            regex_match_type=os.environ["INPUT_REGEX_MATCH_TYPE"],
+        )
 
     def _validate_input(self):
         if self.REGEX_MATCH_TYPE not in self.SUPPORTED_REGEX_MATCH_TYPES:
-            print(f"Regex match type {self.REGEX_MATCH_TYPE} has to be one of {self.SUPPORTED_REGEX_MATCH_TYPES}")
+            print(
+                f"Regex match type {self.REGEX_MATCH_TYPE} has to be one of {self.SUPPORTED_REGEX_MATCH_TYPES}"
+            )
             sys.exit(1)
 
     def _print_result(self, successful: bool):
         body = vars(self)
-        body['MATCH'] = successful
+        body["MATCH"] = successful
         print(json.dumps(body))
         print(f"::set-output name=MATCH::{json.dumps(successful)}")
 
     def run(self):
-        result = getattr(re, self.REGEX_MATCH_TYPE)(pattern=self.REGEX_PATTERN, string=self.TEXT)
+        result = getattr(re, self.REGEX_MATCH_TYPE)(
+            pattern=self.REGEX_PATTERN, string=self.TEXT
+        )
         self._print_result(bool(result))
         sys.exit(0 if result else 1)
 
