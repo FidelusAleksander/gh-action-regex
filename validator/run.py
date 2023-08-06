0e88ef12af7077ab1c5ec49a29611456b48b6d78
MagdalenaBialik <magdalena.bialik@gmail.com>
2023-08-06 18:46:04 +0200

    feat: remove match type argument

--- validator/run.py
+++ validator/run.py
@@ -9,7 +9,6 @@ import os
 action_input = ActionInput(
     regex_pattern=os.environ["INPUT_REGEX_PATTERN"],
     text=os.environ["INPUT_TEXT"],
-    regex_match_type=os.environ["INPUT_REGEX_MATCH_TYPE"],
 )
 action = Action(action_input=action_input)
 
